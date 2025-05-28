710937d65e38dfd0c634d19fbf95435a4ef8550b
ThunderRed1578 <phatle1578@gmail.com>
2025-05-29 01:25:19 +0700

    My book fragment, My favorite fragment, Statistic fragment.

--- app/src/main/java/com/example/librarymanagementsystem/fragment/MyFavoriteFragment.kt
+++ app/src/main/java/com/example/librarymanagementsystem/fragment/MyFavoriteFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FavoriteBookItemFragment.newInstance] factory method to
+ * Use the [MyFavoriteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FavoriteBookItemFragment : Fragment() {
+class MyFavoriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class FavoriteBookItemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_book_item, container, false)
+        return inflater.inflate(R.layout.fragment_my_favorite, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class FavoriteBookItemFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoriteBookItemFragment.
+         * @return A new instance of fragment MyFavoriteFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FavoriteBookItemFragment().apply {
+            MyFavoriteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
