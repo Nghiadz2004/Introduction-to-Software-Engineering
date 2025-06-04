f3c7dc3ccf08366201857a6c867efbcca4fb4563
Nghiadz2004 <huunghia122004@gmail.com>
2025-06-04 23:15:09 +0700

    init manage book backend

--- app/src/main/java/com/example/librarymanagementsystem/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/librarymanagementsystem/adapter/BookAdapter.kt
@@ -1,12 +1,10 @@
 package com.example.librarymanagementsystem.adapter
 
-import android.media.Rating
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.librarymanagementsystem.R
@@ -27,8 +25,7 @@ class BookAdapter(
         val tvTitle: TextView = view.findViewById(R.id.bookTitle)
         val tvAuthor: TextView = view.findViewById(R.id.tvAuthor)
         val tvCategory: TextView = view.findViewById(R.id.bookCategory)
-        val rbRating: RatingBar = view.findViewById(R.id.ratingBar)
-        val tvRaiting: TextView = view.findViewById(R.id.ratingTV)
+        val tvCopyLeft: TextView = view.findViewById(R.id.copyLeftTV)
         val btnEdit: Button = view.findViewById(R.id.editBT)
         val btnRemove: Button = view.findViewById(R.id.removeBT)
     }
@@ -43,10 +40,9 @@ class BookAdapter(
         val book = items[position]
         // holder.imgCover.setImage… (nếu có URL hoặc drawable)
         holder.tvTitle.text = book.title
-        holder.tvAuthor.text = "Author: ${book.author}"
-        holder.tvCategory.text = "Category: ${book.category}"
-        holder.rbRating.rating = book.rating
-        holder.tvRaiting.text = String.format("%.1f", book.rating)
+        holder.tvAuthor.text = book.author
+        holder.tvCategory.text = book.category
+        holder.tvCopyLeft.text = book.quantity.toString()
         holder.btnEdit.setOnClickListener { listener?.onEdit(book) }
         holder.btnRemove.setOnClickListener { listener?.onRemove(book) }
     }
