38025b3c198b26c9918ab3aeeab788f2d38fa540
Ming3993 <minhlvn.t2.1922@gmail.com>
2025-06-07 19:20:39 +0700

    update register logic to create empty favorite list

--- app/src/main/java/com/example/librarymanagementsystem/repository/FavoriteRepository.kt
+++ app/src/main/java/com/example/librarymanagementsystem/repository/FavoriteRepository.kt
@@ -7,6 +7,12 @@ import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class FavoriteRepository(private val db: FirebaseFirestore = FirebaseFirestore.getInstance()) {
+    suspend fun createFavorite(readerId: String, bookIdList: List<String>) = withContext(Dispatchers.IO) {
+        val favorite = Favorite(readerId, bookIdList)
+        db.collection("favorites").document(readerId).set(favorite).await()
+    }
+
+
     // Lấy danh sách Id các quyển sách (bản logic) yêu thích của một độc giả
     suspend fun getFavoriteBooksId(readerId: String): Favorite? = withContext(Dispatchers.IO) {
          db.collection("favorites").document(readerId)
